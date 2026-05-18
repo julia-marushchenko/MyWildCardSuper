b9f8c05c20f836d650ecbe0d724802236c479d5c
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-05-18 20:28:30 +0200

    Add files via upload
    Committing A, B, and Main classes.

---
+++ src/com/mygenerics/Main.java
@@ -0,0 +1,49 @@
+/**
+ *  Java program to create own class with wild card super.
+ */
+
+package com.mygenerics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *  A class.
+ */
+class A {}
+
+/**
+ *  B class.
+ */
+class B extends A {}
+
+/**
+ *  Main class.
+ */
+public class Main {
+
+    // JVM entry point.
+    public static void main(String[] args) {
+
+        // Creating a list with wild card super.
+        List<? super A> list = new ArrayList<>();
+
+        // Adding elements to the list.
+        list.add(new A());
+        list.add(new B());
+        list.add(new A());
+        list.add(new B());
+
+        // Calling method wildCardDemo() for two lists.
+        wildCardDemo(list);
+
+    }
+
+    // Method to print elements of lists with wildcard type.
+    static void wildCardDemo(List<?> list) {
+        for (Object o : list) {
+            System.out.println("The item is: " + o);
+        }
+    }
+}
